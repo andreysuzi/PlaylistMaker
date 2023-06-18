302004fdb9b71abf1400cd77a87e7d03e805e203
andreysuzi <andreysuzi@gmail.com>
2023-06-18 23:55:37 +0300

    Спринт 8.

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.arrow_back)
+
+        backButton.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
